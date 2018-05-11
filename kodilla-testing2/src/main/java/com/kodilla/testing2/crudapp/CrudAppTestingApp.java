44f4d533a7afb608c6a9c2bc32e691b05fdecdb2
“AlanGaleza <alano.gal@gmail.com>
2018-05-11 19:34:25 +0200

    XPath-Position

--- kodilla-testing2/src/main/java/com/kodilla/testing2/crudapp/CrudAppTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/crudapp/CrudAppTestingApp.java
@@ -4,10 +4,12 @@ import com.kodilla.testing2.config.WebDriverConfig;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class CrudAppTestingApp {
     public static final String XPATH_INPUT = "//html/body/main/section/form/fieldset/input";
     public static final String XPATH_TEXTAREA = "//html/body/main/section/form/fieldset[2]/textarea";
+    public static final String XPATH_SELECT = "//select[1]";
 
     public static void main(String[] args) {
         WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.FIREFOX);
@@ -18,5 +20,10 @@ public class CrudAppTestingApp {
 
         WebElement textField = driver.findElement((By.xpath(XPATH_TEXTAREA)));
         textField.sendKeys("The robotic content");
+
+        while (!driver.findElement(By.xpath(XPATH_SELECT)).isDisplayed());
+        WebElement selectCombo = driver.findElement(By.xpath(XPATH_SELECT));
+        Select selectBoard = new Select(selectCombo);
+        selectBoard.selectByIndex(1);
     }
 }
